bad8c4ef75c0a3308022184ce6d8db6c7d1734e9
Martins Onifade <negrosmartins@gmail.com>
2024-09-12 15:56:24 +0100

    dashboard

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./holberton-logo.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div> 
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+        </header>
+      </div>
+      <div>
+        <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        </body>
+      </div>
+      <div>
+        <footer className="App-footer">
+        Copyright 2020 - holberton School
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
