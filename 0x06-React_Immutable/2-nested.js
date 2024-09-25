6d70cf443d70e8e85f37e78f050cbedd8f2c0f76
Martins Onifade <negrosmartins@gmail.com>
2024-09-26 00:48:24 +0100

    nest, list and push

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from "immutable";
+
+export default function accessImmutableObject(object, array) {
+    const mapobj = fromJS(object);
+
+    return mapobj.getIn(array, undefined);
+}
