051ae3f612f68b735aef0a6c2e39cdee7d1ae58d
Martins Onifade <negrosmartins@gmail.com>
2024-09-27 00:58:50 +0100

    3 task

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
-import { fromJS } from "immutable";
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    const mapobj = fromJS(object);
+  const mappedObj = fromJS(object);
 
-    return mapobj.getIn(array, undefined);
+  return mappedObj.getIn(array, undefined);
 }
